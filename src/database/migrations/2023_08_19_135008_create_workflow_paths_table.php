27270eb8465b64bb34fb4bc23fc5ea36112d41e4
Majeed <aabdulmajeed16@gmail.com>
2023-08-19 17:25:22 +0300

    fix db

--- src/database/migrations/2023_08_19_135008_create_workflow_paths_table.php
+++ src/database/migrations/2023_08_19_135008_create_workflow_paths_table.php
@@ -17,10 +17,8 @@ return new class extends Migration
                 ->constrained()
                 ->cascadeOnDelete()
                 ->cascadeOnUpdate();
-            $table->foreignIdFor(\Majeedfahad\Workflower\Models\State::class, 'from_state_id')
-                ->constrained();
-            $table->foreignIdFor(\Majeedfahad\Workflower\Models\State::class, 'to_state_id')
-                ->constrained();
+            $table->foreignIdFor(\Majeedfahad\Workflower\Models\State::class, 'from_state_id');
+            $table->foreignIdFor(\Majeedfahad\Workflower\Models\State::class, 'to_state_id');
             $table->boolean('start')->default(false);
             $table->boolean('end')->default(false);
             $table->timestamps();
